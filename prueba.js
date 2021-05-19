9ae1d228edd9575225a9e94f24a1621936d29c0d
Salvador Cabeza <salvador.cabeza@utp.ac.pa>
2021-05-18 22:59:33 -0500

    terminado ahora si

--- prueba.js
+++ prueba.js
@@ -1,10 +1,11 @@
-
+var palabra= parseInt(prompt("Ingrese una cadena"));
+var binario = palabra.toString(2);
+console.log(binario);
 function texto()
 {
-	var palabra=prompt("Escribe una palabra").toLowerCase();
  
 	// eliminamos los espacios en blanco
-	palabra=palabra.replace(/ /g, "");
+	//palabra=palabra.replace(/ /g, "");
  
 	for (var i=0;i<palabra.length;i++){
 		if(palabra[i]!=palabra[palabra.length-i-1]){
@@ -14,10 +15,30 @@ function texto()
 	return true;
 }
  
-if(texto())
+function texto2()
 {
-	alert("Esto es palíndromo");
-}else{
-	alert("Esto no es palíndromo")
+    
+    
+	// eliminamos los espacios en blanco
+	//binario=binario.replace(/ /g, "");
+ 
+	for (var i=0;i<binario.length;i++){
+		if(binario[i]!=binario[binario.length-i-1]){
+			return false;
+		}
+	}
+	return true;
 }
 
+if(texto())
+{
+	alert("Esto es palíndromo en Base 10");
+}else{
+	alert("Esto no es palíndromo en Base 10")
+}
+
+if (texto2()) {
+    alert("Esto es palíndromo en Base 2");
+}else{
+	alert("Esto no es palíndromo en Base 2")
+}
